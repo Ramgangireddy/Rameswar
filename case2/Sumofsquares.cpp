590954086b73ac547da8f518b6f52f20daa57e12
Ramgangireddy <63634329+Ramgangireddy@users.noreply.github.com>
2020-04-16 14:41:39 +0530

    Create Sumofsquares.cpp

---
+++ case2/Sumofsquares.cpp
@@ -0,0 +1,39 @@
+/*purpose 	:Program for calculating the sum of the squares of a given number
+  filename	:sumofsquares.cpp
+  Author  	:G.v.Rameswara Reddy
+  Date    	:14/04/2020
+*/
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+//Function Name is SumOfSquares
+
+int SumOfSquares()
+{
+		int isum=0;
+		int inum;
+		cout<<"Input	:	";
+		cin>>inum;
+		// Formula for given expression , n(n+1)(2n+1)/6
+		isum = (inum * (inum + 1) * (2 * inum+1)) / 6;   
+		// shows the sum of squares of given number
+   	cout<<"Output	:	"<<isum;	
+}
+
+int main(int argc,char* argv[])
+{
+	//Declaring variables ar required
+   	if((argc==2) && (strcmp(argv[1],"-h")==0))
+	{               
+		{
+		    cout<<"----------Usage Of The File---------------"<<endl;
+			cout<<"Enter any integer value  "<<endl; 
+		}
+	}	
+	else
+	{
+		SumOfSquares();
+	}
+}
